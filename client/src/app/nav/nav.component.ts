5e09798256e5f86e14a2c0cea193e0fcf3351d50
Soumyata Jena <Soumyata.Jena@in.ey.com>
2022-02-23 17:50:14 +0530

    Added LoginService and Reactive LoginForm

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { AccountService } from 'src/_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,7 +9,8 @@ import { FormBuilder } from '@angular/forms';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private fb:FormBuilder) { }
+  loggedIn:boolean;
+  constructor(private fb:FormBuilder,private accountService:AccountService) { }
 
   ngOnInit(): void {
   }
@@ -16,8 +18,18 @@ export class NavComponent implements OnInit {
     userName: [''],
     password: [''],
   });
-  onSubmit() {
+  login() {
     // TODO: Use EventEmitter with form value
     console.warn(this.loginForm.value);
+    this.accountService.login(this.loginForm.value).subscribe(response=>
+      {
+        console.log(response);
+        this.loggedIn=true;
+        console.log(this.loggedIn);
+      },
+      error=>
+      {
+        console.log(error);
+      });
   }
 }
